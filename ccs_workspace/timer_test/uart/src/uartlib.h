eb5d7c7db5a82879f4b93d9189ac1ad10bdf3915
vityha <viktor.povazhuk@ucu.edu.ua>
2022-01-13 21:02:46 +0200

    1. created project with timer example

---
+++ ccs_workspace/timer_test/uart/src/uartlib.h
@@ -0,0 +1,25 @@
+/*
+ * uart_lib.h
+ *
+ *  Created on: Dec 26, 2021
+ *      Author: vityha
+ */
+
+#ifndef UARTLIB_UARTLIB_H_
+#define UARTLIB_UARTLIB_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stdio.h"
+
+void initUart(void);
+void sendUartMsg(const char* str);
+uint8_t stringLength(char* str);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UARTLIB_UARTLIB_H_ */
