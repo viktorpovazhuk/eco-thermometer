a6790a49c91ce26005bdb87c670e450ce2383084
vityha <viktor.povazhuk@ucu.edu.ua>
2022-01-15 12:23:16 +0200

    1. reset gpio

--- ccs_workspace/joined_proj/peripheral/gpiop.cpp
+++ ccs_workspace/joined_proj/peripheral/gpiop.cpp
@@ -9,6 +9,8 @@
 
 void initGpio(void)
 {
+    P1OUT = 0;
+
     //Configure UART pins
     GPIO_setAsPeripheralModuleFunctionInputPin(
     GPIO_PORT_UCA0TXD,
@@ -19,6 +21,7 @@ void initGpio(void)
                                                GPIO_PIN_UCA0RXD,
                                                GPIO_FUNCTION_UCA0RXD);
 
+    // SPI pins
     GPIO_setAsPeripheralModuleFunctionInputPin(
             GPIO_PORT_P2,
             GPIO_PIN4 + GPIO_PIN5 + GPIO_PIN6,
@@ -28,7 +31,13 @@ void initGpio(void)
     P1SEL0 |= BIT2 | BIT3;
     P1SEL1 &= ~(BIT2 | BIT3);
 
-    // for debug
+    // ADC input pin
+    GPIO_setAsPeripheralModuleFunctionInputPin(
+            GPIO_PORT_ADC7,
+            GPIO_PIN_ADC7,
+            GPIO_FUNCTION_ADC7);
+
+    // LEDs for debug
     GPIO_setAsOutputPin(
     GPIO_PORT_P1,
                         GPIO_PIN1);
